ecbff36466a5c586671d2c3d21ef2a1c6c071b75
Nasir Saeed <84667154+nasirsaeed2704@users.noreply.github.com>
2023-02-18 12:28:03 +0000

    about me

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_world():
-  return "<h1>hello world! all good</h1>"
+  return render_template('home.html')
 
 if __name__ == "__main__":
   app.run('0.0.0.0', debug = True)
